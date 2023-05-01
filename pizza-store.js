0691dc2ff52ccb8f54d891f4fcf4d95de4455416
Clayton <127148272+Clay2002@users.noreply.github.com>
2023-05-01 12:10:21 -0400

    Update 3.0
    I added a lot of javascript on this commit. I just added some arrays with basic functions to do the math for the total order of the 4 pizzas you can select. I also worked out some of the functions for the tip that will take the total and let you pick a tip. I got that working in the console but not the HTML rn cause I want to add drinks and wings to the total options and then I'll add the tip you selected into the total. Same with The driver fee.

--- pizza-store.js
+++ pizza-store.js
@@ -5,7 +5,16 @@
 const container1  = document.getElementById("container-1")
 const container2  = document.getElementById("container-2")
 const container3  = document.getElementById("container-3")
+const container4  = document.getElementById("container-4")
 
+//tip
+const tip10 = document.getElementById("10");
+const tip15 = document.getElementById("15");
+const tip20 = document.getElementById("20");
+
+
+
+//pizza items
 const itemOne = document.getElementById("item-1")
 const itemTwo = document.getElementById("item-2")
 const itemThree = document.getElementById("item-3")
@@ -14,8 +23,126 @@ const itemFour = document.getElementById("item-4")
 
 
 
+
+
+
+
+
+const total = document.getElementById("cart-total")
+
+
+
 const cart = document.getElementById("cart");
 
 
 
 
+
+container3.remove();
+
+
+var price = ["", "","",""];
+var tip = ["","",""];
+
+
+
+
+//add to cart for pizzas
+function Addcart1(){
+
+
+    price[0] = 15;
+
+    total.innerText = price[0] + price[1] + price[2] + price[3];
+   
+
+    console.log(price);
+
+};
+
+function Addcart2(){
+
+
+
+    price[1] = 17;
+
+    total.innerText = price[0] + price[1] + price[2] + price[3];
+   
+    console.log(price);
+
+  
+
+
+};
+function Addcart3(){
+
+    price[2] = 20;
+
+    total.innerText = price[0] + price[1] + price[2] + price[3];
+   
+    console.log(price);
+
+  
+
+
+};
+
+function Addcart4(){
+
+    price[3] = 13;
+
+    total.innerText = price[0] + price[1] + price[2] + price[3];
+   
+    console.log(price);
+
+
+
+
+};
+
+
+
+//tip functions
+
+function tip1(){
+
+    tip[0] = total.innerText * (10/100);
+    
+    console.log(tip[0]);
+
+    console.log(tip);
+}
+
+function tip2(){
+
+    tip[1] = total.innerText * (15/100);
+    
+    console.log(tip[1]);
+
+    console.log(tip);
+}
+
+function tip3(){
+
+    tip[2] = total.innerText * (20/100);
+    
+    console.log(tip[2]);
+
+    console.log(tip);
+}
+
+
+console.log(tip);
+
+console.log(price);
+
+
+//wings functions
+
+
+
+//drinks functions
+
+
+
+var priceD = ["","","",""];
