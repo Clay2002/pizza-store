5d6180c92274b04be9b7e61de9c1d6289652959d
Clayton <127148272+Clay2002@users.noreply.github.com>
2023-05-07 19:21:06 -0400

    Update 6.0
    Added a lot in the javascript dom. Spend 2 hours just going through what I want when you click order. I give the client 2 options to order on the card or with cash. The cash option displays some text and the credit card will display a lot of user inputs with options to type in them. I need to add a lot more to the credit card page by adding if-else statements to each input and also making the order button on that page work and will open another page saying the order has been completed. After that, I have some basic cool ideas I want to add. The main thing I will work on after all of that will be the homepage and responsive design. I have a lot of work for this week.

--- pizza-store.js
+++ pizza-store.js
@@ -7,11 +7,100 @@ function payNow(){
     container3.remove();
     container4.remove();
 
+
+
+
     let stuff = document.getElementById("payout");
 
-    stuff.innerHTML = "New Heading";
+    stuff.innerHTML = "What payment will you be using?";
+    var btn1 = document.createElement("BUTTON");
+    var text1 = document.createTextNode("Credit Card");
+    btn1.appendChild(text1);
+    document.body.appendChild(btn1);
+    btn1.id ='btn1'; 
+
+    var btn2 = document.createElement("BUTTON");
+    var text2 = document.createTextNode("Cash");
+    btn2.appendChild(text2);
+    document.body.appendChild(btn2);
+    btn2.id ='btn2'; 
+
+
+
+//cash option
+    btn2.onclick = function(){
+
+        btn1.remove();
+        btn2.remove();
+        stuff.remove();
+
+
+        
+var cashdisplay = document.getElementById('cashdisplay')
+
+
+cashdisplay.innerHTML = "Please have the cash amount ready when we arrive!";
+time.innerHTML = "Your Order is cooking in the oven!";
+time2.innerHTML = "30 mins till order is finished!";
+
+
+    }
+
+
+
+ //credit card option
+    btn1.onclick = function() {
+       
+       
+        btn2.remove();
+        btn1.remove();
+        stuff.remove();
+
+        const name = document.getElementById("name")
+        const AccountNumber = document.getElementById("AccountNumber")
+        
+    
+        name.innerHTML = "Enter First And Last Name";
+        const input1 = document.createElement("INPUT");
+        input1.id ='input1'; 
+        document.body.appendChild(input1);
+
+
+        AccountNumber.innerHTML = "Enter Account Number";
+        const input2 = document.createElement("INPUT");
+        input2.id ='input2'; 
+        document.body.appendChild(input2);
+
+        ExpirationDate.innerHTML = "Enter Expiration Date";
+        const input3 = document.createElement("INPUT");
+        input3.id ='input3'; 
+        document.body.appendChild(input3);
+
+        CVC.innerHTML = "Enter CVC";
+        const input4 = document.createElement("INPUT");
+        input4.id ='input4'; 
+        document.body.appendChild(input4);
+        
+        var btnOrder = document.createElement("BUTTON");
+        var text3 = document.createTextNode("Order Now!");
+        btnOrder.appendChild(text3);
+        document.body.appendChild(btnOrder);
+        btnOrder.id ='btnOrder'; 
+
+
+    };
+
+   
+
+
 
 };
+
+
+
+
+
+
 
 const container1  = document.getElementById("container-1")
 const container2  = document.getElementById("container-2")
